f91e6974f4306c0d9e73b5d5f0ddf20fd6f74ec9
Daivuk <daivuk@gmail.com>
2022-11-02 17:49:55 -0400

    Fix

--- ReddyGame/src/main.cpp
+++ ReddyGame/src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <Engine/ReddyEngine.h>
 
-int main(int argc, char** argv)
+int main(int argc, const char** argv)
 {
     auto pGame = std::make_shared<Game>();
     Engine::Run(pGame, argc, argv);
---
+++ assets/README.md
@@ -0,0 +1,1 @@
+Final asset files: PNG, WAV, etc
