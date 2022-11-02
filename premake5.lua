6314f93d9c2e902aec4fdf1c717440e6e9be8ade
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-11-02 22:50:12 +0100

    Add imgui

--- premake5.lua
+++ premake5.lua
@@ -21,6 +21,7 @@ workspace "ReddyGame"
 group "Dependencies"
 	include "premake"
    include "ReddyEngine/SDL2-Premake"
+   include "ReddyEngine/imgui"
 group ""
 include "ReddyEngine"
 include "ReddyGame"
@@ -29,3 +30,4 @@ include "ReddyGame"
 
 IncludeDir = {}
 IncludeDir["SDL2"] = "%{wks.location}/ReddyEngine/SDL2-Premake/include"
+IncludeDir["imgui"] = "%{wks.location}/ReddyEngine/imgui"
