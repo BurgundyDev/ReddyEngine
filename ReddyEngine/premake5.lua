169e4f3c8aaa84cba720e84601e7d66e9cb55236
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-11-03 16:29:52 +0100

    Update premake5.lua

--- ReddyEngine/premake5.lua
+++ ReddyEngine/premake5.lua
@@ -8,49 +8,49 @@ project "ReddyEngine"
    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
    files
-   {
-      "src/**.h",
-      "src/**.hpp",
-      "src/**.c",
-      "src/**.cpp",
-      "include/**.h",
-      "include/**.hpp",
-      "include/**.c",
-      "include/**.cpp"
-   }
+   	{
+		"src/**.h",
+		"src/**.hpp",
+	  	"src/**.c",
+	  	"src/**.cpp",
+	  	"include/**.h",
+	  	"include/**.hpp",
+	  	"include/**.c",
+	  	"include/**.cpp"
+   	}
 
    includedirs
 	{
 		"src",
 		"include",
-      "%{IncludeDir.stb}",
-      "%{IncludeDir.SDL2}",
-      "%{IncludeDir.glm}",
-      "%{IncludeDir.imgui}",
-      "%{IncludeDir.jsoncpp}",
-      "%{IncludeDir.lib_json}",
-      "%{IncludeDir.tinyxml2}",
-      "%{IncludeDir.box2d}",
-      "%{IncludeDir.box2d_source}"
+	  	"%{IncludeDir.stb}",
+	  	"%{IncludeDir.SDL2}",
+	  	"%{IncludeDir.glm}",
+	  	"%{IncludeDir.imgui}",
+	  	"%{IncludeDir.jsoncpp}",
+	  	"%{IncludeDir.lib_json}",
+	  	"%{IncludeDir.tinyxml2}",
+	  	"%{IncludeDir.box2d}",
+	  	"%{IncludeDir.box2d_source}"
 	}
 
    links
 	{
-      "ImGui",
-      "SDL2",
-      "jsoncpp",
-      "tinyxml2",
-      "box2d",
+	  	"ImGui",
+	  	"SDL2",
+	  	"jsoncpp",
+	  	"tinyxml2",
+	  	"box2d",
 		"opengl32.lib"
 	}
 
    filter "configurations:Debug"
-      defines { "DEBUG" }
-      runtime "Release"
-      runtime "Debug"
-      symbols "On"
+	  defines { "DEBUG" }
+	  runtime "Release"
+	  runtime "Debug"
+	  symbols "On"
 
    filter "configurations:Release"
-      defines { "RELEASE" }
-      symbols "Off"
-      optimize "On"
+	  defines { "RELEASE" }
+	  symbols "Off"
+	  optimize "On"
