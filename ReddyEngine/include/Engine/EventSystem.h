e9477dc00cdddff9430ce24d79ad297e23a15bcc
Stradek <stradowski.stradek@gmail.com>
2022-11-08 16:40:28 +0100

    Fix some problems create another ones
    I will fix it at home. Just leaving some ideas.

--- ReddyEngine/include/Engine/EventSystem.h
+++ ReddyEngine/include/Engine/EventSystem.h
@@ -16,20 +16,24 @@ namespace Engine
 	{
 	private:
 		typedef void* EventHandler;
-		
+
 		std::queue<IEvent*> m_pEventQueue;
-		std::map<std::type_index, std::vector<std::function<void (IEvent*)>>> m_pEventHandlersMap;
+		std::map<std::type_index, std::vector<std::function<void(IEvent*)>>> m_pEventHandlersMap;
 	public:
 		EventSystem(); // remove later
 		~EventSystem();
 
 		void dispatchEvents();
 
-		void queueEvent(IEvent* e);
+		template <typename T>
+		void queueEvent(Event<T>* e)
+		{
+			m_pEventQueue.push(e);
+		}
 		void queueEvent(SDL_Event* e);
 
 		template<typename T>
-		void registerListener(EventHandler instance,  std::function<void(IEvent*)> callback)
+		void registerListener(EventHandler instance,  std::function<void (IEvent*)> callback)
 		{
 			std::type_index id = typeid(T);
 			//if (m_pEventHandlersMap[id].size() == 0)
@@ -41,7 +45,7 @@ namespace Engine
 		template<typename T>
 		void deregisterListener(EventHandler instance)
 		{
-			std::vector<EventHandler>& callbacks = m_pEventHandlersMap[eventType];
+			std::vector<std::function<void(IEvent*)>>& callbacks = m_pEventHandlersMap[eventType];
 
 			for (auto it = callbacks.begin(); it < it->end(); it++)
 			{
