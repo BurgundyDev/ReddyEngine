08a6e3765509b558956e7588e8b20a9f9c320a70
Daivuk <daivuk@gmail.com>
2022-11-13 10:30:16 -0500

    Fixed font

--- ReddyEngine/src/Font.cpp
+++ ReddyEngine/src/Font.cpp
@@ -49,7 +49,7 @@ namespace Engine
         fclose(fontFile);
 
         // Create dynamic texture where we will put in the glyphs
-        pRet->m_pAtlas = Texture::createDynamic({ATLAS_SIZE, ATLAS_SIZE}, TextureFormat::R8);
+        pRet->m_pAtlas = Texture::createDynamic({ATLAS_SIZE, ATLAS_SIZE});
 
         // Init packing
         pRet->m_pRectContext = new stbrp_context();
@@ -81,8 +81,8 @@ namespace Engine
     
     Font::Font()
     {
-        m_pAtlasData = new uint8_t[ATLAS_SIZE * ATLAS_SIZE];
-        memset(m_pAtlasData, 0, ATLAS_SIZE * ATLAS_SIZE);
+        m_pAtlasData = new uint8_t[ATLAS_SIZE * ATLAS_SIZE * 4];
+        memset(m_pAtlasData, 0, ATLAS_SIZE * ATLAS_SIZE * 4);
         m_pInfo = new stbtt_fontinfo();
     }
 
@@ -105,12 +105,14 @@ namespace Engine
         // get bounding box for character
         int c_x1, c_y1, c_x2, c_y2;
         stbtt_GetCodepointBitmapBox(m_pInfo, codepoint, m_scale, m_scale, &c_x1, &c_y1, &c_x2, &c_y2);
+        int w = c_x2 - c_x1;
+        int h = c_y2 - c_y1;
 
         // Find space in our atlas
         stbrp_rect rect;
         rect.id = codepoint;
-        rect.w = (c_x2 - c_x1) + PADDING * 2;
-        rect.h = (c_y2 - c_y1) + PADDING * 2;
+        rect.w = w + PADDING * 2;
+        rect.h = h + PADDING * 2;
         stbrp_pack_rects(m_pRectContext, &rect, 1);
 
         if (!rect.was_packed)
@@ -120,8 +122,29 @@ namespace Engine
         }
         
         // render character (stride and offset is important here)
-        int byteOffset = (rect.x + PADDING) + ((rect.y + PADDING) * ATLAS_SIZE);
-        stbtt_MakeCodepointBitmap(m_pInfo, m_pAtlasData + byteOffset, c_x2 - c_x1, c_y2 - c_y1, ATLAS_SIZE, m_scale, m_scale, codepoint);
+        //int byteOffset = (rect.x + PADDING) + ((rect.y + PADDING) * ATLAS_SIZE);
+        //stbtt_MakeCodepointBitmap(m_pInfo, m_pAtlasData + byteOffset, w, h, ATLAS_SIZE, m_scale, m_scale, codepoint);
+        static std::vector<uint8_t> charData;
+        charData.resize(w * h);
+        stbtt_MakeCodepointBitmap(m_pInfo, charData.data(), w, h, w, m_scale, m_scale, codepoint);
+
+        for (int srcy = 0; srcy < h; ++srcy)
+        {
+            int dsty = rect.y + PADDING + srcy;
+            for (int srcx = 0; srcx < w; ++srcx)
+            {
+                int dstx = rect.x + PADDING + srcx;
+                int srck = srcy * w + srcx;
+                int dstk = ((dsty * ATLAS_SIZE) + dstx) * 4;
+                uint8_t c = charData[srck];
+
+                // Premultiplied
+                m_pAtlasData[dstk + 0] = c;
+                m_pAtlasData[dstk + 1] = c;
+                m_pAtlasData[dstk + 2] = c;
+                m_pAtlasData[dstk + 3] = c;
+            }
+        }
 
         // Create the character
         Char* chr = new Char();
